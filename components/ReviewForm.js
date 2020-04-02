76e993ce1cf0fe9566d43b4ad035924b0099f1c1
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-04-01 17:27:30 -0700

    tweak inputs

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -8,60 +8,75 @@ const ReviewForm = () => {
 
   return (
     <View>
-      <AntDesign name="form" size={32} color="green" />
+      <AntDesign name="form" size={32} color="darkgray" />
       <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        businessOrganization: '',
-        typeOfBusiness: '',
-        email: '',
-        phone: '',
-        payment: ''
-      }}
-      onSubmit={(values) => {
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          businessOrganization: '',
+          typeOfBusiness: '',
+          email: '',
+          phone: '',
+          payment: ''
+        }}
+        onSubmit={(values) => {
 
-      }}
-      >
-      {(formikProps) => (
-        <View>
-          <TextInput
-            placeholder='First Name'
-            onChangeText={formikProps.handleChange('firstName')}
-            value={formikProps.values.firstName}
-            />
-          <TextInput
-            placeholder='Last Name'
-            onChangeText={formikProps.handleChange('lastName')}
-            value={formikProps.values.lastName}
-            />
-          <TextInput
-            placeholder='Business / Organization'
-            onChangeText={formikProps.handleChange('businessOrganization')}
-            value={formikProps.values.businessOrganization}
-            />
+        }}
+        >
+        {(formikProps) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder='First Name'
+              onChangeText={formikProps.handleChange('firstName')}
+              value={formikProps.values.firstName}
+              />
+            <TextInput
+              style={styles.input}
+              placeholder='Last Name'
+              onChangeText={formikProps.handleChange('lastName')}
+              value={formikProps.values.lastName}
+              />
+            <TextInput
+              style={styles.input}
+              placeholder='Business / Organization'
+              onChangeText={formikProps.handleChange('businessOrganization')}
+              value={formikProps.values.businessOrganization}
+              />
             <TextInput
               multiline
+              style={styles.input}
               placeholder='Type of Business'
               onChangeText={formikProps.handleChange('typeOfBusiness')}
               value={formikProps.values.typeOfBusiness}
               />
-              <TextInput
-                placeholder='Email'
-                onChangeText={formikProps.handleChange('email')}
-                value={formikProps.values.email}
-                />
-                <TextInput
-                  placeholder='phone'
-                  onChangeText={formikProps.handleChange('phone')}
-                  value={formikProps.values.phone}
-                  />
+            <TextInput
+              style={styles.input}
+              placeholder='Email'
+              onChangeText={formikProps.handleChange('email')}
+              value={formikProps.values.email}
+              />
+            <TextInput
+              style={styles.input}
+              placeholder='phone'
+              onChangeText={formikProps.handleChange('phone')}
+              value={formikProps.values.phone}
+              />
 
-                <Button title='Submit' color='red' onPress={formikProps.handleSubmit}/>
-        </View>
-      )}
+            <Button title='Submit' backgroundColor='blue' color='red' onPress={formikProps.handleSubmit}/>
+          </View>
+        )}
       </Formik>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 60
+  },
+});
+
+
+
 export default ReviewForm
